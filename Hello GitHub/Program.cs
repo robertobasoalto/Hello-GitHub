2b10b5a6ce2af4a2c6ad28e724ec351414c9b9c7
TheBobBot <robertobasoalto@hotmail.com>
2020-07-11 20:26:48 +0100

    Added the following fields: GreetingsGitHub , GreetingsNewBranch

--- Hello GitHub/Program.cs
+++ Hello GitHub/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,10 +9,12 @@ namespace Hello_GitHub
 {
     class Program
     {
+        private static string GreetingGitHub = "Hello GitHub";
+        private static string GreetingsNewBranch = "Hello new branch";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello GitHub");
-            Console.WriteLine("Hello new branch");
+            Console.WriteLine(GreetingGitHub);
+            Console.WriteLine(GreetingsNewBranch);
             Console.ReadKey();
         }
     }
