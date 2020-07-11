7d9435c4a1da274808a9cbe5b4d34154453e27ba
TheBobBot <robertobasoalto@hotmail.com>
2020-07-11 20:34:22 +0100

    Fixed field name spelling: GreetingGitHub to GreetingsGitHub

--- Hello GitHub/Program.cs
+++ Hello GitHub/Program.cs
@@ -9,11 +9,11 @@ namespace Hello_GitHub
 {
     class Program
     {
-        private static string GreetingGitHub = "Hello GitHub";
+        private static string GreetingsGitHub = "Hello GitHub";
         private static string GreetingsNewBranch = "Hello new branch";
         static void Main(string[] args)
         {
-            Console.WriteLine(GreetingGitHub);
+            Console.WriteLine(GreetingsGitHub);
             Console.WriteLine(GreetingsNewBranch);
             Console.ReadKey();
         }
